ea56528ad8c9f0ace70771e91359a5a43e9d2b18
basaksamir3 <basaksamir3@gmail.com>
2021-09-24 22:14:42 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,24 @@
+// falsy
+    // 0
+    // false
+    // ""
+    // null
+    // undefined
+    // NaN
+//truthy
+    // " "
+    // "0"
+    // "null"
+    // []
+    // {}
+    // "NaN"
+
+let names = "NaN";
+
+
+if (names) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
